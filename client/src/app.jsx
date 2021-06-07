2b05e7f7bc7510cf6299e95f32abf91e37ecd9b1
Warren Wong <warren.wong4@gmail.com>
2021-06-07 11:13:03 -0400

    Project environment Setup
    Create initial project files and separated modules for different features.

---
+++ client/src/app.jsx
@@ -0,0 +1,18 @@
+import React from "react";
+require("dotenv").config();
+
+export default class App extends React.Component {
+  constructor(props) {
+    super(props);
+  }
+
+  render() {
+    return (
+      <main>
+        <header>
+          <h1>Hello World</h1>
+        </header>
+      </main>
+    );
+  }
+}
