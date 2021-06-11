035342e205fbd1f272a25c057af704718f5635a2
Warren Wong <warren.wong4@gmail.com>
2021-06-11 11:02:11 -0400

    Modify QA component to store Search Component State
    Implemented a searchChangeHandler passed down one level via props to Search component in order to store the query string at top componenent level.
    
    Subsequently the stored state will be used by the question list view component to filter based off the query.

--- client/src/components/qa/QA.jsx
+++ client/src/components/qa/QA.jsx
@@ -1,13 +1,19 @@
-import React, { useContext } from "react";
+import React, { useContext, useState } from "react";
 import Search from "./components/Search.jsx";
 import { QaContext } from "../../store.jsx";
 
 export default function QA() {
+  const [query, setQuery] = useState("");
   const qa = useContext(QaContext);
+
+  const searchChangeHandler = (e) => {
+    setQuery(e.target.value);
+  };
+
   return (
     <>
       <h4>{"Questions & Answers"}</h4>
-      <Search />
+      <Search search={searchChangeHandler} />
     </>
   );
 }
