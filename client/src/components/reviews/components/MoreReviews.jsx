b65d600dd582963c79943984bed2b3385b21f70d
Mark-Vale <markpvale@gmail.com>
2021-06-14 23:23:57 -0400

    Fix format in MoreReviews Component

--- client/src/components/reviews/components/MoreReviews.jsx
+++ client/src/components/reviews/components/MoreReviews.jsx
@@ -6,28 +6,25 @@ import CallMoreReviews from '../api/CallMoreReviews';
 const MoreReviews = () => {
 
   const onClickInputChange = () => {
-
-    const response = CallMoreReviews.get('/reviews/', {
+    const response = CallMoreReviews.get("/reviews/", {
       params: {
         count: 100,
         product_id: 11001,
       }
     })
-    .then((response) => {
-      console.log(response.data.results);
-    })
-    .catch((err) => {
-      console.log(err);
-    })
-
+      .then((response) => {
+        console.log(response.data.results);
+      })
+      .catch((err) => {
+        console.log(err);
+      })
   };
 
   return (
     <>
-      <button onClick={onClickInputChange}> Click Me </button>
+      <button onClick={onClickInputChange}> More Reviews </button>
     </>
-  )
-
+  );
 };
 
 export default MoreReviews;
